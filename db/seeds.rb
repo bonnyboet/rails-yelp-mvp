4f37fd6d722c31e36a30d2e74b1cbdb5d58c973a
Bonny Boet <bonny.boet@gmail.com>
2022-07-05 21:20:29 +0900

    Add restaurant model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Loading current database'
+puts '...'
+puts 'Emptying database ...'
+Restaurant.destroy_all
+puts 'Data base is now empty. Preparing new entries.'
+
+100.times do
+  @restaurant = Restaurant.new
+  @restaurant.name = Faker::Restaurant.name
+  @restaurant.address = Faker::Address.street_address
+  @restaurant.phone_number = Faker::PhoneNumber.cell_phone_in_e164
+  @restaurant.category = ['chinese', 'italian', 'Japanese', 'french', 'belgian'].sample
+  @restaurant.save
+end
+
+@restaurants = Restaurant.all
+@restaurants.each do |r|
+  puts "#{r.id} - #{r.name} #{r.address} #{r.category}"
+end
+
+puts 'Successfully added 100 fake entries to the database'
